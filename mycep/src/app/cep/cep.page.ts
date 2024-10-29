2334673bf75a613b0d1b4d0b081203f141dcee1c
Matheus Bomfim <matheus.bomfim@uol.com.br>
2024-10-29 20:36:21 -0300

    comentario

--- mycep/src/app/cep/cep.page.ts
+++ mycep/src/app/cep/cep.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http'; // NECESSARIO ADICIONAR MANUAMENTE NO APP.MODULE.TS NOS IMPORTS
 
 
 @Component({
